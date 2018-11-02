b6e8875c75199ce92dba7d163d51846831f86344
CINDIA WINARTA <i16015@ftis>
2018-11-02 14:38:34 +0700

    Min

--- Calculator.java
+++ Calculator.java
@@ -25,7 +25,12 @@ public class Calculator {
     }
     
     public float division(float a, float b){
-		
         return a/b;
     }
+	
+	public float min(float a, float b){
+		if(a>b){
+			return b;
+		}else return a;
+	}
 }
