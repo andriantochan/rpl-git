8a916e02004b6963746c51b0ee6091b5e99645e9
ANDRIANTO CHANDRA <i16017@ftis>
2018-11-02 13:37:35 +0700

    Halo

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i16017
+ */
+public class Calculator {
+    
+    public Calculator(){};
+    
+    public float plus(float a, float b){
+        return 0;
+    }
+                
+    public float minus(float a, float b){
+        return 0;
+    }
+    
+    public float multiply(float a, float b){
+        return 0;
+    }
+    
+    public float division(float a, float b){
+        return 0;
+    }
+}
