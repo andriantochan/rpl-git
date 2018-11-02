66252755730b13b2c6b0f5a7e1e90eb62f93ce43
ANDRIANTO CHANDRA <i16017@ftis>
2018-11-02 13:53:12 +0700

    Halo

--- Calculator.java
+++ Calculator.java
@@ -13,11 +13,11 @@ public class Calculator {
     public Calculator(){};
     
     public float plus(float a, float b){
-        return a+b;
+        return 0;
     }
                 
     public float minus(float a, float b){
-        return a-b;
+        return 0;
     }
     
     public float multiply(float a, float b){
